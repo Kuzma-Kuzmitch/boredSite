b0e8bdafe70a3f24ce1a8b3e779c9f7ff586d309
Evan Kuzma <evankuzma@gmail.com>
2018-08-01 23:47:43 -0400

    initial design

---
+++ script/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+
+$("#contactButton").on("click", function() {
+  $("#contactCard").toggle("slow")
+  $("#fishIcon").toggleClass("fa-flip-horizontal")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
