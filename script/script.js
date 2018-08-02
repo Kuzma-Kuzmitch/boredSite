b98318a6058c774cbe6687e05c37b501168ea01b
Evan Kuzma <evankuzma@gmail.com>
2018-08-02 16:22:00 -0400

    changed about section

--- script/script.js
+++ script/script.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
 
 
 $("#contactButton").on("click", function() {
+  if ($("#aboutCard").is(":visible")) {
+    $("#aboutCard").toggle("slow")
+  }
   $("#contactCard").toggle("slow")
+  $("#fishIcon").toggleClass("fa-flip-horizontal")
+})
+
+$("#aboutButton").on("click", function() {
+
+  if ($("#contactCard").is(":visible")) {
+    $("#contactCard").toggle("slow")
+  }
+  $("#aboutCard").toggle("slow")
   $("#fishIcon").toggleClass("fa-flip-horizontal")
 })
 
